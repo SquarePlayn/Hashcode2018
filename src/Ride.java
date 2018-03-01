1995115bdd604c6a639c105822f3639aed628c50
Chess <34745596+Chessnl@users.noreply.github.com>
2018-03-01 19:00:52 +0100

    Add files via upload
    Ride Class

---
+++ src/Ride.java
@@ -0,0 +1,15 @@
+
+public class Ride {
+	
+	int a, b, x, y, start, finish;
+	
+	Ride(int a, int b, int x, int y, int start, int finish) {
+		this.a = a;
+		this.b = b;
+		this.x = x;
+		this.y = y;
+		this.start = start;
+		this.finish = finish;
+	}
+
+}
