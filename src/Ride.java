a6163f6f1f3d99f9cd89324b5ca976100fb32fe4
Chess <34745596+Chessnl@users.noreply.github.com>
2018-03-01 19:32:16 +0100

    Update Ride.java

--- src/Ride.java
+++ src/Ride.java
@@ -1,15 +1,22 @@
-
 public class Ride {
 	
-	int a, b, x, y, start, finish;
+	int rideID, a, b, x, y, start, finish, drivingDistance;
 	
-	Ride(int a, int b, int x, int y, int start, int finish) {
+	
+	Ride(int rideID, int a, int b, int x, int y, int start, int finish) {
+		this.rideID = rideID;
 		this.a = a;
 		this.b = b;
 		this.x = x;
 		this.y = y;
 		this.start = start;
 		this.finish = finish;
+		drivingDistance = Math.abs(a - x) + Math.abs(b - y);
 	}
-
+	
+	
+	
+	
+	
+	
 }
